a3d8e7e0c84d7b5da887d67651405e414f87586f
dylanito2 <dylanito2@gmail.com>
2017-04-12 18:01:35 -0400

    added active column

--- poke-racer-api/db/schema.rb
+++ poke-racer-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412214448) do
+ActiveRecord::Schema.define(version: 20170412215850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170412214448) do
     t.integer  "creator_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "is_active"
   end
 
   add_foreign_key "account_races", "accounts"
