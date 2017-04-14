8aaf7203682067c00e839dce9f9a0c23f20bb034
github username <github email address>
2017-04-14 11:28:22 -0400

    working on racelist

---
+++ poke-racer-react/src/components/Race/RaceList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class RaceList extends React {
+
+}
+
+const mapStateToProps = (state) => {
+  return  (
+  account: state.Account
+  races: state.Race
+  )
+}
