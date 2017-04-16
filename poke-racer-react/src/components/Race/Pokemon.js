76903817463f0544ea3604cbe02124fae32c052e
Noah Schechter <noahschechter7@gmail.com>
2017-04-16 10:39:52 -0400

    finalized race for now

--- poke-racer-react/src/components/Race/Pokemon.js
+++ poke-racer-react/src/components/Race/Pokemon.js
@@ -1,11 +1,21 @@
 class Pokemon {
-	constructor(image, xCoordsAtPercentiles) {
+	constructor(image, xCoordsAtPercentiles, yCoord) {
 		this.image = image
 		this.xCoordsAtPercentiles = xCoordsAtPercentiles
+		this.pace = 1
+		this.xCoord = 0
+		this.yCoord = yCoord
+		this.currentPercentile = 10
+		Pokemon.all.push(this)
 	}
 
 	getImage() {
 		return this.image
+	}
+
+	setCoords(x, y) {
+		this.xCoord = x
+		this.yCoord = y
 	}
 
 	// {0: 10}
@@ -21,9 +31,33 @@ class Pokemon {
 			return currentPercentile + 10
 	}
 
+	static updatePaceForPokemon(currentPercentile) {
+		for (let i = 0; i < Pokemon.getAll().length; i++) {
+			let temp = Pokemon.all[i]
+			temp.pace = (temp.getXCoordAtPercentile(currentPercentile) - temp.getXCoordAtPercentile(currentPercentile - 10)) / currentPercentile
+		}
+	}
+
+	static drawAll(ctx, time) {
+		for (let i = 0; i < Pokemon.getAll().length; i++) {
+			let tempPokemon = Pokemon.all[i]
+			if (tempPokemon.getNextPercentile(tempPokemon.currentPercentile) == null) {
+				ctx.drawImage(tempPokemon.getImage(), tempPokemon.xCoord, tempPokemon.yCoord)
+			} else if (tempPokemon.xCoord < tempPokemon.getXCoordAtPercentile(tempPokemon.getNextPercentile(tempPokemon.currentPercentile))) {
+				tempPokemon.xCoord = tempPokemon.xCoord + tempPokemon.pace
+				ctx.drawImage(tempPokemon.getImage(), tempPokemon.xCoord, tempPokemon.yCoord)
+			} else {
+				tempPokemon.currentPercentile = tempPokemon.getNextPercentile(tempPokemon.currentPercentile + 10)
+				tempPokemon.pace = (tempPokemon.getXCoordAtPercentile(tempPokemon.currentPercentile) - tempPokemon.getXCoordAtPercentile(tempPokemon.currentPercentile - 10)) / tempPokemon.currentPercentile
+			}
+		}
+	}
+
 	static getAll() {
 		return Pokemon.all
 	}
 }
+
+Pokemon.all = []
 
 export default Pokemon
--- poke-racer-react/src/components/Race/Race.js
+++ poke-racer-react/src/components/Race/Race.js
@@ -21,93 +21,32 @@ class Race extends Component {
 		{
 			10: 30, 20: 300, 30: 350, 40: 400, 
 			50: 500, 60: 600, 70: 610, 80: 620, 90: 900, 100: 1000
-		})
+		}, 100)
 		this.pokemonI = new Pokemon(this.ivysaur, 
 		{
 			10: 100, 20: 300, 30: 350, 40: 400, 
-			50: 500, 60: 600, 70: 610, 80: 620, 90: 700, 100: 980
-		})
-
+			50: 500, 60: 600, 70: 610, 80: 620, 90: 920, 100: 980
+		}, 200)
+		this.pokemonV = new Pokemon(this.venu, 
+		{
+			10: 100, 20: 200, 30: 300, 40: 400, 
+			50: 500, 60: 600, 70: 700, 80: 800, 90: 900, 100: 990
+		}, 300)
 		this.drawCanvas = this.drawCanvas.bind(this)
-		this.raceImage = this.raceImage.bind(this)
-		this.raceImages = this.raceImages.bind(this)
-		this.racePokemon = this.racePokemon.bind(this)
-		this.racePokemonList = this.racePokemonList.bind(this)
+		this.startRace = this.startRace.bind(this)
 	}
 
-	racePokemon(ctx, time, pokemon, yCoord) {
+	startRace(ctx, time, pokemonList) {
 		let currentPercentile = 10
-		let xCoord = 0
-		let pace = pokemon.getXCoordAtPercentile(currentPercentile) / currentPercentile
 		let stop = setInterval(() => {
-			if (pokemon.getNextPercentile(currentPercentile) == null) {
-				clearInterval(stop)
-			} else if (xCoord < pokemon.getXCoordAtPercentile(pokemon.getNextPercentile(currentPercentile))) {
-				ctx.clearRect(xCoord, yCoord, 100, 100)
-				xCoord = xCoord + pace
-				ctx.drawImage(pokemon.getImage(), xCoord, yCoord)
-			} else {
-				currentPercentile = pokemon.getNextPercentile(currentPercentile + 10)
-				pace = (pokemon.getXCoordAtPercentile(currentPercentile) - pokemon.getXCoordAtPercentile(currentPercentile - 10)) / currentPercentile
-			}
-		}, 100)
-	}
-
-	// 30 / 10 = 3
-	// 270 / 20 = 13.5
-	// 50 / 30 = 1.667
-	// 50 / 40 = 1.25
-	// 100 / 50 = 2
-	// 100 / 60 = 1.667
-	// 10 / 70 = 0.14
-	// 10 / 80 = 0.125
-	// 280 / 90 = 3.11
-	// 100 / 100 = 1
-
-
-	raceImage(ctx, time, image, yCoord) {
-		let xCoord = 0
-		let stop = setInterval(() => {
-	    	ctx.drawImage(image, xCoord, yCoord)
-	    	xCoord = xCoord + 1
-	    	if (xCoord > this.state.finishLineXCoord)
-	    		clearInterval(stop)
-		}, 10)
-	}
-
-	raceImages(ctx, time, images, yCoordStart) {
-		let xCoord = 0
-		let stop = setInterval(() => {
-	    	// ctx.drawImage(image, xCoord, yCoord)
-	    	this.drawImages(ctx, images, xCoord, yCoordStart, 50)
-	    	xCoord = xCoord + 1
-	    	if (xCoord > this.state.finishLineXCoord)
-	    		clearInterval(stop)
+			ctx.clearRect(0, 0, 1500, 1500)
+			Pokemon.drawAll(ctx, time)
 		}, 10)
 	}
 
 	drawCanvas({ctx, time}) {
 		const {width, height} = ctx.canvas
-	    // ctx.save()
-	    // ctx.clearRect(0, 0, width, height)
-	    // ctx.fillStyle = 'black'
-	    // ctx.translate(width / 2, height / 2)
-	    // ctx.rotate(((time / 10) % 360) * Math.PI / 180)
-	    // ctx.fillRect(-1 * width / 4, -1 * height / 4, width / 2, height / 2)
-	    // ctx.restore()
-	    // this.raceImage(ctx, time, this.bulbasaur, 100)
-	    // this.raceImage(ctx, time, this.ivysaur, 200)
-	    // this.raceImage(ctx, time, this.venu, 300)
-	    //this.raceImages(ctx, time, [this.bulbasaur, this.ivysaur, this.venu], 100)
-	    // this.racePokemon(ctx, time, this.pokemonB, 200)
-	    // this.racePokemon(ctx, time, this.pokemonI, 100)
-	    this.racePokemonList(ctx, time, [this.pokemonB, this.pokemonI], 100, 100)
-	}
-
-	racePokemonList(ctx, time, pokemonList, yCoordStart, yCoordOffSet) {
-		for (let i = 0; i < pokemonList.length; i++) {
-			this.racePokemon(ctx, time, pokemonList[i], yCoordStart + (i * yCoordOffSet))
-		}
+	    this.startRace(ctx, time, [this.pokemonB, this.pokemonI, this.pokemonV])
 	}
 
 	render() {
